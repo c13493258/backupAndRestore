4e618e31a607e7c4f42094f08a836ec771bd6a9d
c13493258 <c13493258@gmail.com>
2016-04-18 15:37:18 +0100

    Second commit

--- backup.sh
+++ backup.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+echo "backup files on your pc in a compressed format "
+echo "Type backup for a backup of files or type restore for restoration of files "
+read option
+if [ $option = "backup" ];then
+	echo "full or incremental backup"
+	read choice
+	if [ $choice = "full" ];then
+
+		echo "attempting backup"
+		cd /
+		mkdir /home/$USER/Desktop/backupstuff
+		rsync --progress --delete -aE --exclude=/home/$USER/Desktop/backupfiles --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude=/lost+found /* /home/$USER/Desktop/backupstuff
+	
+ elif [ $option = "restore" ];then
+	echo "attempting to restore"
+else
+	echo "no argument passed "
+fi
